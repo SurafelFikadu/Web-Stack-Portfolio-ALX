e6ece4e3d96014c285fcd622a1a134a14161f2f7
SurafelFikadu <surafelfikadu05@gmail.com>
2024-09-18 20:58:26 +0300

    first commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./Hero.css"
+import hand_icon from '../Assets/hand_icon.png'
+import arrow_icon from '../Assets/arrow.png'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className='hero-left'>
+            <h2>FOR NEW ARRIVALS ONLY</h2>
+            <div className='hero-hand-icon-1'>
+                <div className="hero-hand-icon">
+                    <p>BestWatch</p>
+                    <img src={hand_icon} alt="" />
+                    <p>collection</p>
+                </div>
+                
+                <p>for every one</p>
+            </div>
+            <div className='hero-latest-btn'>
+                <div>Latest Collection</div>
+                <img src={arrow_icon} alt="" />
+            </div>
+        </div>
+        <div className="hero-right">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
+import NewCollecton from '../Components/NewCollection/NewCollecton'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
+
+const Shop = () => {
+  return (
+    <div>
+        <Hero />
+        <Popular />
+        <Offers />
+        <NewCollecton />
+        <NewsLetter />
+    </div>
+  )
+}
+
+export default Shop
