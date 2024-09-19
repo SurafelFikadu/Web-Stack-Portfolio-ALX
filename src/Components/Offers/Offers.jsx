886b98c46959483abe76c8d5d32ab333a471f38f
SurafelFikadu <surafelfikadu05@gmail.com>
2024-09-19 09:27:21 +0300

    ...

--- src/Components/Offers/Offers.jsx
+++ src/Components/Offers/Offers.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import './Offers.css'
-// import exclusive_image from '../Assets/exclusive_image.png'
 
 const Offers = () => {
   return (
     <div className='offers'>
         <div className="offers-left">
             <h1>Exclusive Offers For You</h1>
-            <h1></h1>
             <p>ONLY ON THE BEST SELLERS PRODUCTS</p>
             <button>Check Now</button>
         </div>
